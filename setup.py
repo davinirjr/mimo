7007826a67743f0444c762f2175fc42f831883a5
Liam Childs <liam_childs@hotmail.com>
2016-09-19 15:24:13 +0200

    added MANIFEST.in

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import os
 
 from setuptools import setup, find_packages
 
-long_description = open('README.rst').read() if os.path.exists('README.rst') else\
-    open('README.md').read()
+with open('README.rst', encoding='utf-8') if os.path.exists('README.rst') else \
+        open('README.md', encoding='utf-8') as fileobj:
+    long_description = fileobj.read()
 
 setup(
     name='mimo',
-    version='1.0.1',
     author='Liam H. Childs',
     author_email='liam.h.childs@gmail.com',
     packages=find_packages(exclude=['tests']),
@@ -15,5 +15,9 @@ setup(
     license='LICENSE.txt',
     description='A streaming multi-input, multi-output Python library',
     long_description=long_description,
-    install_requires=['lhc-python']
+    install_requires=['lhc-python'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics']
 )
